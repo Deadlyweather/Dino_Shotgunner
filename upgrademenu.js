009d28188b2c2ef416c3acd4f8fef4763c73296c
ibizaa2 <111568546+ibizaa2@users.noreply.github.com>
2026-04-16 22:28:57 +0300

    upgrademenu päivitys

--- upgrademenu.js
+++ upgrademenu.js
@@ -12,19 +12,46 @@ class UpgradeMenu {
         this.lihaOpen = false;
         this.neulaOpen = false;
 
-        this.upgrades = {
+        this.hoveredItem = null;
+
+  this.upgrades = {
     liha: [
-        { id: "hp", name: "Max HP", amount: 0, cost: 10 },
-        { id: "regen", name: "HP Regen", amount: 0, cost: 20 },
-        { id: "armor", name: "Armor", amount: 0, cost: 50 },
-        
+        { id: "maxhp", name: "Max HP", cost: 10, amount: 0, currency: "meat", stats: { maxhp: 10, hp: 10, size: 0.1, agility: -1 } },
+        { id: "vitality", name: "Vitality", cost: 100, amount: 0, currency: "meat", stats: { vitality: 1, metabolism: 10 } },
+        { id: "defence", name: "Defence", cost: 250, amount: 0, currency: "meat", stats: { defence: 1 } },
+        { id: "agility", name: "Agility", cost: 500, amount: 0, currency: "meat", stats: { agility: 1, metabolism: -10 } },
+        { id: "strength", name: "Strength", cost: 500, amount: 0, currency: "meat", stats: { strenght: 1, metabolism: 10 } },
+        { id: "maxsat", name: "Max Fud", cost: 1000, amount: 0, currency: "meat", stats: { maxsaturation: 10, saturation: 10, metabolism: -10 } },
+        { id: "luck", name: "Luck", cost: 1500, amount: 0, currency: "meat", stats: { luck: 1 } },
+        { id: "endurance", name: "Endurance", cost: 2500, amount: 0, currency: "meat", stats: { endurance: 1, defence: 1 } },
+        { id: "wings", name: "Wings", cost: 3500, amount: 0, currency: "meat", stats: { jumps: 1 } },
+        { id: "gluttony", name: "Gluttony", cost: 3500, amount: 0, currency: "meat", stats: { gluttony: 1 } },
+        { id: "vampirism", name: "Vampirism", cost: 6666, amount: 0, currency: "meat", stats: { vampirism: 1 } }
     ],
-    neula: [
-        { id: "dmg", name: "Damage", amount: 0, cost: 15 },
-        { id: "speed", name: "Reload", amount: 0, cost: 25 },
-        { id: "ammo", name: "More Ammo", amount: 0, cost: 25 },
-    ]
+   neula: [
+   
+    { id: "dmg", name: "Damage", cost: 20, amount: 0, currency: "needles", stats: { firepower: 1, firerateMax: 10, spread: 10, ammoCost: 1 } },
+    { id: "firerate", name: "Fire Rate", cost: 50, amount: 0, currency: "needles", stats: { firerateMax: -10, loadMax: 10 } },
+    { id: "reload", name: "Quick Load", cost: 50, amount: 0, currency: "needles", stats: { loadMax: -10, loadAmount: 1, range: -25 } },
+    { id: "bullets", name: "Bullets", cost: 200, amount: 0, currency: "needles", stats: { volume: 1, spread: 10, usage: 1 } },
+    { id: "ammo", name: "Ammo Box", cost: 500, amount: 0, currency: "needles", stats: { ammoCost: -1, maxammo: 1, loadMax: 400 } },
+    { id: "range", name: "Range", cost: 750, amount: 0, currency: "needles", stats: { range: 50, loadMax: 10, ammoCost: 1 } },
+    { id: "pierce", name: "Pierce", cost: 1000, amount: 0, currency: "needles", stats: { pierce: 1, firepower: -1 } },    
+    { id: "automation", name: "Automation", cost: 2500, amount: 0, currency: "needles", stats: { autoload: 1, loadMax: 100, loadAmount: -1 } },
+    { id: "compression", name: "Compression", cost: 2500, amount: 0, currency: "needles", stats: { spread: -10, usage: -1, firepower: 1, loadMax: 10, volume: -3, range: -50 } },
+    { id: "smartammo", name: "Smart Ammo", cost: 2500, amount: 0, currency: "needles", stats: { usage: -1, ammoCost: -1 } },
+    { id: "heftyammo", name: "Hefty Ammo", cost: 2500, amount: 0, currency: "needles", stats: { pierce: 1, firepower: 1, loadMax: 10, firerateMax: 10 } },
+    { id: "inflatable", name: "Inflat. Mag", cost: 2500, amount: 0, currency: "needles", stats: { maxammo: 50, loadMax: 20 } },
+    { id: "acceleration", name: "Acceleration", cost: 4500, amount: 0, currency: "needles", stats: { loadMax: -1000, loadAmount: 10, autoload: -1 } },
+    { id: "sniper", name: "Sniper gun", cost: 7500, amount: 0, currency: "needles", stats: { range: 500, firepower: 5, loadMax: 1000, ammoCost: 10 } },
+    { id: "supershotgun", name: "Super gun", cost: 9999, amount: 0, currency: "needles", stats: { firepower: 25, volume: 10, loadMax: 400, autoload: -1, usage: 5 } }
+]
 };
+
+window.addEventListener("mousemove", (e) => {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+    });
 
 window.addEventListener("wheel", (e) => {
     if (!this.isOpen) return;
@@ -36,7 +63,7 @@ window.addEventListener("wheel", (e) => {
     if (this.scrollY > 0) this.scrollY = 0;
 
    
-    if (this.scrollY < -500) this.scrollY = -500; 
+    if (this.scrollY < -1000) this.scrollY = -1000; 
 });
 
 window.addEventListener("mousedown", (e) => {
@@ -59,11 +86,8 @@ window.addEventListener("mousedown", (e) => {
             let itemHeight = 40; 
 
             if (this.isInside(mouseX, mouseY, itemX, itemY - 20, itemWidth, itemHeight)) {
-                item.amount++;
-                this.player.upgrades++
-               if(item.id ==="hp"){
-                this.player.maxhp += 10;
-               }
+                this.useUpgrade(item);
+
                
             }
         });
@@ -76,13 +100,9 @@ window.addEventListener("mousedown", (e) => {
             let itemHeight = 40;
 
            
-            if (this.isInside(mouseX, mouseY, itemX, itemY - 20, itemWidth, itemHeight)) {
-                item.amount++;
-                this.player.upgrades++
+               if (this.isInside(mouseX, mouseY, itemX, itemY - 20, itemWidth, itemHeight)) {
+                this.useUpgrade(item);
 
-                if(item.id === "ammo"){
-                    this.player.ammo++;
-                }
                
             }
         });
@@ -91,14 +111,50 @@ window.addEventListener("mousedown", (e) => {
 
     }
 
+    useUpgrade(item){
+        const currency = item.currency
+
+        /*if (this.player[currency] < item.cost){
+            return
+        }*/ 
+        this.player[currency] -= item.cost
+     
+        item.amount++
+        this.player.upgrades++
+
+        if (item.stats){
+
+            for (let stat in item.stats){
+                const changedValue = item.stats[stat];
+                
+                if (stat === "size" && this.player.size >= 4){
+                    continue
+                }
+
+                if (stat === "defence"){
+                    if (this.player.defence >= 99){
+                        continue
+                    }
+                }
+
+            
+
+                this.player[stat] += changedValue
+            }
+    
+        }
+            
+    }
+
     drawNeulaList(ctx, x, y){
     this.upgrades.neula.forEach((item, index) => {
         let itemY = y + 150 + index * 60 + this.scrollY;
         let itemX = x + 550;
         let itemXRight = x + 950;
+       if (this.isInside(this.mouseX, this.mouseY, itemX, itemY - 20, 400, 40)){
+            this.hoveredItem = item;
+        }
 
-        
-        
         ctx.textAlign = "left";
         ctx.fillText(item.name, itemX, itemY);
 
@@ -113,6 +169,14 @@ window.addEventListener("mousedown", (e) => {
     drawLihaList(ctx, x, y) {
     this.upgrades.liha.forEach((item, index) => {
         let itemY = y + 150 + index * 60 + this.scrollY;
+     
+
+        if (this.isInside(this.mouseX, this.mouseY, x + 50, itemY - 20, 400, 40)){
+            this.hoveredItem = item;
+            
+        
+        }
+      
         
         
         ctx.textAlign = "left";
@@ -136,6 +200,70 @@ window.addEventListener("mousedown", (e) => {
            pointY >= yläReuna && pointY <= alaReuna;
 }
 
+drawTooltip(ctx) {
+        const item = this.hoveredItem;
+        if (!item) return;
+
+        ctx.save()
+        const mouseXx = this.mouseX + 15
+        const mouseYy = this.mouseY + 15
+        const boxW = 340
+        
+        let statsCount = 0
+        for (let s in item.stats){
+            statsCount++
+        } 
+        const boxH = statsCount * 30 + 20
+
+        ctx.fillStyle = "rgba(0, 0, 0, 0.95)";
+        ctx.fillRect(mouseXx, mouseYy, boxW, boxH);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(mouseXx, mouseYy, boxW, boxH);
+
+        ctx.font = "14px arial";
+        ctx.textAlign = "left";
+
+        const positiveGoodStats = ["maxammo", "vampirism","firepower", "volume", "range", "pierce", "maxhp", "hp", "vitality", "defence", "agility", "strenght", "luck", "endurance", "jumps", "gluttony", "vampirism", "loadAmount", "autoload", "maxsaturation", "saturation"];
+        const NegativeGoodStats = ["metabolism", "firerateMax", "loadMax", "spread", "usage", "ammoCost", "size"]
+        let row = 0;
+        for (let stat in item.stats) {
+            const change = item.stats[stat]
+            const current = this.player[stat]
+            const next = current + change;
+
+         
+            let color;
+            if (positiveGoodStats.includes(stat)) {
+                if (change > 0) {
+                    color = "#aaffaa";
+                } else {
+                    color = "#ffaaaa"; 
+                }
+            } else if (NegativeGoodStats.includes(stat)) {
+                if (change < 0) {
+                    color = "#aaffaa"; 
+                } else {
+                    color = "#ffaaaa";
+                }
+            }
+            
+            ctx.fillStyle = color;
+           
+           
+     
+
+    let text = stat + ": " + current + " -> " + next;
+
+
+    let textX = mouseXx + 15;
+    let textY = mouseYy + 30 + (row * 25);
+
+    ctx.fillText(text, textX, textY);
+
+    row++
+        }
+        ctx.restore();
+    }
 drawMainMenu(ctx, x, y){
     ctx.fillStyle = "darkred";
     ctx.fillRect(x, y, this.width / 2, this.height);
@@ -158,16 +286,22 @@ drawMainMenu(ctx, x, y){
     ctx.beginPath();
     ctx.moveTo(x, y + 100); 
 
-    ctx.lineTo(x + this.width, y + 100);
+    ctx.lineTo(x + this.width, y + 100); 
+
+    ctx.fillStyle = "black";
+    ctx.font = "bold 30px arial";
+    
+
+   
+    
+    
+    ctx.textAlign = "left"; 
+    ctx.fillText("Liha: " + Math.floor(this.player.meat), x + this.width * 0.25, y + 60);
 
     ctx.stroke();
-    ctx.fillStyle = "black"
-    ctx.font = "bold 30px  arial";
-    ctx.textAlign = "center";
-    ctx.fillText("Liha", x + this.width * 0.25, y + 60 );
+    ctx.textAlign = "left";
+    ctx.fillText("Neula: " + Math.floor(this.player.needles), x + this.width * 0.75, y + 60);
 
-    ctx.stroke();
-    ctx.fillText("Neula", x + this.width * 0.75, y + 60 );
 
 
 
@@ -203,6 +337,7 @@ draw(ctx) {
     const y = (ctx.canvas.height - 700) / 2;
 
     this.drawMainMenu(ctx, x, y);
+    this.hoveredItem = null;
 
     if (this.currentView === "hp_list"){
         ctx.beginPath();
@@ -212,6 +347,10 @@ draw(ctx) {
 
         this.drawLihaList(ctx, x, y);
         this.drawNeulaList(ctx, x, y);
+    }
+
+    if(this.hoveredItem){
+        this.drawTooltip(ctx);
     }
     
 
